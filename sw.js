d1b3f3db0acd4147c31cc1bf66a96d4ee2f333ee
PapaMike80 <70916601+PapaMike80@users.noreply.github.com>
2026-08-14 19:21:16 +0200

    Aggiorna cache PWA per import ODS

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 
 // Cambio obbligatorio dopo il ripristino dello script popup: invalida anche
 // le copie memorizzate dalla PWA e da Safari.
-const CACHE_VERSION = 'navisuite-v157-ods-navi-parser';
+const CACHE_VERSION = 'navisuite-v158-ods-riposo';
 const CACHE_NAME = CACHE_VERSION;
 
 // File statici da pre-caricare durante l'installazione.
